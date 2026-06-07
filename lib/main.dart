bc57e24a585034e1bd9b15d21d726716202c0da9
gojo1777 <gojomovie0@gmail.com>
2026-06-07 23:05:05 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'YouTube',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+      ),
+      home: const YouTubeScreen(),
+    );
+  }
+}
+
+class YouTubeScreen extends StatefulWidget {
+  const YouTubeScreen({super.key});
+
+  @override
+  State<YouTubeScreen> createState() => _YouTubeScreenState();
+}
+
+class _YouTubeScreenState extends State<YouTubeScreen> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setUserAgent(
+        'Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 Chrome/120 Mobile Safari/537.36'
+      )
+      ..loadRequest(Uri.parse('https://m.youtube.com'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(controller: _controller),
+      ),
+    );
+  }
+}
